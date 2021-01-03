8124fee77e0acdeddf4e05a1a040adf84569e00f
star <54599456+star-1337@users.noreply.github.com>
2021-01-03 10:37:44 +0000

    Fixed output message for cogs

--- DiscordSuggestionBot/main.py
+++ DiscordSuggestionBot/main.py
@@ -8,23 +8,23 @@ client = commands.Bot(command_prefix= "s!")
 @commands.is_owner()
 async def load (ctx, extension):
     client.load_extension(f"cogs.{extension}")
-    await ctx.send(">cog loaded")
+    await ctx.send("> cog loaded")
 
 @client.command()
 @commands.is_owner()
 async def unload (ctx, extension):
     client.unload_extension(f"cogs.{extension}")
-    await ctx.send(">cog unloaded")
+    await ctx.send("> cog unloaded")
 
 @client.command()
 @commands.is_owner()
 async def reload (ctx, extension):
     client.unload_extension(f"cogs.{extension}")
     client.load_extension(f"cogs.{extension}")
-    await ctx.send(">cog reloaded")
+    await ctx.send("> cog reloaded")
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f'cogs.{filename[:-3]}')
-
+        
 client.run("TOKEN")
